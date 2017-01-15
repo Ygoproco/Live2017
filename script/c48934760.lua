17d3ce788a1611ef62d489702a8c7d4cb7dae908
edo9300 <edoardo762@gmail.com>
2017-01-15 22:00:41 +0100

    Update c48934760.lua

--- script/c48934760.lua
+++ script/c48934760.lua
@@ -32,7 +32,7 @@ function c48934760.activate(e,tp,eg,ep,ev,re,r,rp)
 		end
 		local fc2=Duel.GetFieldCard(1-tp,LOCATION_SZONE,5)
 		if fc2 and Duel.IsDuelType(DUEL_OBSOLETE_RULING) then
-			Duel.SendtoGrave(fc2,REASON_RULE)
+			Duel.Destroy(fc2,REASON_RULE)
 			Duel.BreakEffect()
 		end
 		Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true)
