a0a39ec817d82bf175fdfb37f9916ad30ceeb3e7
EerieCode <eeriecode@gmail.com>
2017-02-08 21:54:39 +0100

    Removed scripts for old beta cards

--- script/c100911081.lua
+++
@@ -1,6 +0,0 @@
---Sea Monster of Theseus
---Scripted by Eerie Code
-function c100911081.initial_effect(c)
-	c:EnableReviveLimit()
-	aux.AddFusionProcFunRep(c,aux.FilterBoolFunction(Card.IsFusionType,TYPE_TUNER),2,true)
-end
