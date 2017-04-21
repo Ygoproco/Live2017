c69c04ca8eb72fddb480d8b23aaafd4df49a3e10
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-04-21 13:06:13 -0300

    small fix for Stronghold Whale
    small detail in spop

--- script/c100418016.lua
+++ script/c100418016.lua
@@ -49,7 +49,10 @@ end
 function c100418016.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
-	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsLocation(LOCATION_HAND) then
+		Duel.SendtoGrave(c,REASON_RULE)
+	end
 end
 function c100418016.filter(c)
 	return c:IsCode(100418018) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSSetable()
