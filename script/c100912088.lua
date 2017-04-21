d1c4d3623ad2eedb6c255cbccbaa93037423ec02
EerieCode <eeriecode@gmail.com>
2017-04-21 10:05:25 +0200

    New cards and bug fixes (#205)
    * Added new cards (credits to dest)
    
    Giant Rex
    Ultra Evolution
    Spacetime Transcendence
    
    * Fixed "The Legendary Fisherman II"
    
    * Added "SPYRAL MISSION - Rescue"
    
    * Added "SPYRAL GEAR - Fully Armed"
    
    * Fixed "SPYRAL GEAR - Fully Armed"
    
    Credits to cybercatman

---
+++ script/c100912088.lua
@@ -0,0 +1,74 @@
+-- SPYRAL Gear – Fully Armed
+-- Scripted by cybercatman
+function c100912088.initial_effect(c)
+	aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsCode,41091257))
+	--atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(1000)
+	c:RegisterEffect(e2)
+	--damage
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(100912088,0))
+	e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BATTLE_DESTROYING)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCondition(c100912088.rmcon)
+	e3:SetTarget(c100912088.rmtg)
+	e3:SetOperation(c100912088.rmop)
+	c:RegisterEffect(e3)
+	--special summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetDescription(aux.Stringid(100912088,1))
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetCondition(c100912088.spcon)
+	e4:SetTarget(c100912088.sptg)
+	e4:SetOperation(c100912088.spop)
+	c:RegisterEffect(e4)
+end
+function c100912088.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	local ec=e:GetHandler():GetEquipTarget()
+	if not (ec and eg:IsContains(ec)) then return false end
+	local bc=ec:GetBattleTarget()
+	e:SetLabelObject(bc)
+	return bc:IsLocation(LOCATION_GRAVE) and bc:IsReason(REASON_BATTLE) and bc:IsAbleToRemove() 
+		and Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,bc)
+end
+function c100912088.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,2,0,0)  
+end
+function c100912088.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local bc=e:GetLabelObject()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD,1,1,bc)
+	if g:GetCount()==0 then return end
+	g:Merge(bc) 
+	Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+end
+function c100912088.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_DESTROY) and c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousPosition(POS_FACEUP)
+end
+function c100912088.filter(c,e,tp)
+	return c:IsCode(41091257) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c100912088.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c100912088.filter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c100912088.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c100912088.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c100912088.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
