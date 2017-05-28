c574f2fe966a3078ca199bd7552b072cb0a6e569
TheOnePharaoh <alaaz018@yahoo.com>
2017-05-28 10:50:33 -0600

    Fixed Cards that copy the Original name and effects
    Q.
    スターヴ・ヴェノム・フュージョン・ドラゴン
    相手が同じモンスターに（2）の効果を与えた場合。 ループを作成できますか？ または、1ターンに1度はまだ有効ですか？
    A.
    相手フィールドの「スターヴ・ヴェノム・フュージョン・ドラゴン」を対象に、「スターヴ・ヴェノム・フュージョン・ドラゴン」の『②』の効果を発動する事はできません。
    
    Q.
    Stave, Venom, Fusion, Dragon
    When the opponent gives effect (2) to the same monster. Can I create a loop? Or is it still valid once per turn?
    A.
    You can not activate the effect of "②" of "Stave, Venom, Fusion, Dragon" for the opponent's field "Stave, Venom, Fusion, Dragon".
    
    Ruling provided by Steeldarkeagel
    
    Note:
    Red Supremecy already applies that ruling and it's not a BKSS ruling because
    `.......the effect text says `this card's name becomes that target's original name, and .......`
    the name can't become the target's original name if it already is with that same name
    it's the same reason you can't target a monster with 0 ATK and (no negatable effect or if it's effects are already negated) using Clear Wing Fast Dragon
    also the same reason you can't declare the current level in the effect of gagaga magician

---
+++ script/c4068622.lua
@@ -0,0 +1,89 @@
+--BF－極光のアウロラ
+function c4068622.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(c4068622.spcon)
+	e1:SetOperation(c4068622.spop)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(4068622,0))
+	e2:SetCategory(CATEGORY_REMOVE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(c4068622.target)
+	e2:SetOperation(c4068622.operation)
+	c:RegisterEffect(e2)
+	--special summon limit
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e3:SetValue(aux.FALSE)
+	c:RegisterEffect(e3)
+	c:EnableReviveLimit()
+end
+function c4068622.spfilter1(c,ft,tp)
+	if c:GetSequence()<5 then ft=ft+1 end
+	return c:IsFaceup() and c:IsSetCard(0x33) and c:IsType(TYPE_TUNER) and c:IsAbleToRemove()
+		and ft>-1 and Duel.IsExistingMatchingCard(c4068622.spfilter2,tp,LOCATION_MZONE,0,1,nil,ft)
+end
+function c4068622.spfilter2(c,ft)
+	return c:IsFaceup() and not c:IsType(TYPE_TUNER) and c:IsAbleToRemove() and (ft>0 or c:GetSequence()<5)
+end
+function c4068622.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	return ft>-2 and Duel.IsExistingMatchingCard(c4068622.spfilter1,tp,LOCATION_MZONE,0,1,nil,ft,tp)
+end
+function c4068622.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g1=Duel.SelectMatchingCard(tp,c4068622.spfilter1,tp,LOCATION_MZONE,0,1,1,nil,ft,tp)
+	local tc=g1:GetFirst()
+	if tc:GetSequence()<5 then ft=ft+1 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g2=Duel.SelectMatchingCard(tp,c4068622.spfilter2,tp,LOCATION_MZONE,0,1,1,nil,ft)
+	g1:Merge(g2)
+	Duel.Remove(g1,POS_FACEUP,REASON_COST)
+end
+function c4068622.filter(c)
+	return c:IsSetCard(0x33) and c:IsAbleToRemove() and (c:GetCode()~=code or c:GetBaseAttack()>0)
+end
+function c4068622.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c4068622.filter,tp,LOCATION_EXTRA,0,1,nil,e:GetHandler():GetCode()) and e:GetHandler():GetFlagEffect(4068622)==0 end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,LOCATION_EXTRA)
+end
+function c4068622.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,c4068622.filter,tp,LOCATION_EXTRA,0,1,1,nil,e:GetHandler():GetCode()) 
+	local tc=g:GetFirst()
+	local c=e:GetHandler()
+	if tc and c:IsFaceup() and c:IsRelateToEffect(e) and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0 then
+		local code=tc:GetOriginalCode()
+		local ba=tc:GetBaseAttack()
+		local reset_flag=RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END
+		c:CopyEffect(code,reset_flag,1)
+		e:GetHandler():RegisterFlagEffect(4068622,reset_flag,0,1)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(reset_flag)
+		e1:SetCode(EFFECT_CHANGE_CODE)
+		e1:SetValue(code)
+		c:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetReset(reset_flag)
+		e2:SetCode(EFFECT_SET_BASE_ATTACK)
+		e2:SetValue(ba)
+		c:RegisterEffect(e2)
+	end
+end
