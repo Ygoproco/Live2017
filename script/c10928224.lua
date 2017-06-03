a5733c0b76238dd1b639bed9ee901f37035718e3
EerieCode <eeriecode@gmail.com>
2017-06-03 10:52:49 +0200

    Official numbers
    Duelist Pack: Legend Duelist

--- script/c10928224.lua
+++ script/c10928224.lua
@@ -1,19 +1,19 @@
 --アマゾネスペット仔虎
 --Amazoness Pet Baby Tiger
 --Scripted by cybercatman
-function c100418009.initial_effect(c)
+function c10928224.initial_effect(c)
 	--spsummon
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(100418009,0))
+	e1:SetDescription(aux.Stringid(10928224,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
-	e1:SetCountLimit(1,100418009)
-	e1:SetCondition(c100418009.spcon)
-	e1:SetTarget(c100418009.sptg)
-	e1:SetOperation(c100418009.spop)
+	e1:SetCountLimit(1,10928224)
+	e1:SetCondition(c10928224.spcon)
+	e1:SetTarget(c10928224.sptg)
+	e1:SetOperation(c10928224.spop)
 	c:RegisterEffect(e1)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -32,13 +32,13 @@ function c100418009.initial_effect(c)
 	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e4:SetCode(EFFECT_UPDATE_ATTACK)
 	e4:SetRange(LOCATION_MZONE)
-	e4:SetValue(c100418009.val)
+	e4:SetValue(c10928224.val)
 	c:RegisterEffect(e4)
 	--limit
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_SINGLE)
 	e5:SetCode(EFFECT_CANNOT_SUMMON)
-	e5:SetCondition(c100418009.sumcon)
+	e5:SetCondition(c10928224.sumcon)
 	c:RegisterEffect(e5)
 	local e6=e5:Clone()
 	e6:SetCode(EFFECT_CANNOT_FLIP_SUMMON)
@@ -47,29 +47,29 @@ function c100418009.initial_effect(c)
 	e7:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
 	c:RegisterEffect(e7)
 end
-function c100418009.cfilter(c,tp)
+function c10928224.cfilter(c,tp)
 	return c:IsFaceup() and c:IsControler(tp) and c:IsSetCard(0x4)
 end
-function c100418009.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(c100418009.cfilter,1,nil,tp)
+function c10928224.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c10928224.cfilter,1,nil,tp)
 end
-function c100418009.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c10928224.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function c100418009.spop(e,tp,eg,ep,ev,re,r,rp)
+function c10928224.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 end
-function c100418009.val(e,c)
+function c10928224.val(e,c)
 	return Duel.GetMatchingGroupCount(Card.IsSetCard,c:GetControler(),LOCATION_GRAVE,0,nil,0x4)*100
 end
-function c100418009.sumfilter(c)
+function c10928224.sumfilter(c)
 	return c:IsFaceup() and c:GetOriginalCode()==10979723
 end
-function c100418009.sumcon(e)
+function c10928224.sumcon(e)
 	local c=e:GetHandler()
-	return Duel.IsExistingMatchingCard(c100418009.sumfilter,c:GetControler(),LOCATION_MZONE,0,1,c)
+	return Duel.IsExistingMatchingCard(c10928224.sumfilter,c:GetControler(),LOCATION_MZONE,0,1,c)
 end
