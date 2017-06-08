02f850f7576c6b00d52e1d6c90096799ec41d70b
TheOnePharaoh <alaaz018@yahoo.com>
2017-06-08 09:50:19 -0600

    Fixed Alure Of Darkness and Simular cards Fixed for cards that can't be removed from play and cards that prevent removal

--- script/c40465719.lua
+++ script/c40465719.lua
@@ -1,27 +1,27 @@
---闇の誘惑
-function c1475311.initial_effect(c)
+--凡人の施し
+function c40465719.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(c1475311.target)
-	e1:SetOperation(c1475311.activate)
+	e1:SetTarget(c40465719.target)
+	e1:SetOperation(c40465719.activate)
 	c:RegisterEffect(e1)
 end
-function c1475311.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function c40465719.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,EFFECT_CANNOT_REMOVE) and Duel.IsPlayerCanDraw(tp,2) end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(2)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
 end
-function c1475311.activate(e,tp,eg,ep,ev,re,r,rp)
+function c40465719.activate(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT)
 	Duel.BreakEffect()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(p,Card.IsAttribute,p,LOCATION_HAND,0,1,1,nil,ATTRIBUTE_DARK)
+	local g=Duel.SelectMatchingCard(p,Card.IsType,p,LOCATION_HAND,0,1,1,nil,TYPE_NORMAL)
 	local tg=g:GetFirst()
 	if tg then
 		if Duel.Remove(tg,POS_FACEUP,REASON_EFFECT)==0 then
