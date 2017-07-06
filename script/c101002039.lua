cc341e920070e296eb20b7a3e12cfb8862054fd4
EerieCode <eeriecode@gmail.com>
2017-07-06 21:16:05 +0200

    Added new cards
    Blastant
    Elemental Grace Doriado
    Burning Bamboo Sword

--- script/c101002039.lua
+++ script/c101002039.lua
@@ -2,7 +2,7 @@
 --Elemental Grace Doriado
 --Scripted by Eerie Code
 function c101002039.initial_effect(c)
-	c:EnableReviveLimit()
+	c:EnableReviveLimit() 
 	--cannot special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
