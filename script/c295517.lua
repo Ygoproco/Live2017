af2a32336c028715c28f9a1ba3f1dffb5d7bf3b7
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-08-07 21:55:03 +0800

    A Legendary Ocean fix on ATK/DEF change

--- script/c295517.lua
+++ script/c295517.lua
@@ -11,7 +11,7 @@ function c295517.initial_effect(c)
 	e2:SetCode(EFFECT_UPDATE_LEVEL)
 	e2:SetRange(LOCATION_FZONE)
 	e2:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,LOCATION_HAND+LOCATION_MZONE)
-	e2:SetTarget(aux.TargetBoolFunction(c295517.filter))
+	e2:SetTarget(c295517.tg)
 	e2:SetValue(-1)
 	c:RegisterEffect(e2)
 	--Atk
@@ -19,12 +19,13 @@ function c295517.initial_effect(c)
 	e3:SetCode(EFFECT_UPDATE_ATTACK)
 	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
 	e3:SetValue(200)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_WATER))
 	c:RegisterEffect(e3)
 	--Def
 	local e4=e3:Clone()
 	e4:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e4)
 end
-function c295517.filter(c)
+function c295517.tg(e,c)
 	return c:IsLevelAbove(2) and c:IsAttribute(ATTRIBUTE_WATER)
 end
