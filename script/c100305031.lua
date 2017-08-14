f525c1433f84871d2420df7b83a2a12e6e354965
EerieCode <eeriecode@gmail.com>
2017-08-14 18:18:43 +0200

    Bug fixes (#511)
    * Bug fixes
    
    Mythical Beast Jackal
    Reincarnating Parshath
    Apprentice Illusion Magician
    Magical Boost
    
    * Fixed "Reincarnating Parshath"

--- script/c100305031.lua
+++ script/c100305031.lua
@@ -21,12 +21,15 @@ function c100305031.cfilter(c)
 end
 function c100305031.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(c100305031.cfilter,tp,LOCATION_HAND,0,1,e:GetHandler())
-		and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler())
+		and (Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) 
+			or Duel.IsPlayerAffectedByEffect(tp,EFFECT_DISCARD_COST_CHANGE))
 		and Duel.CheckLPCost(tp,1000) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
 	local cg=Duel.SelectMatchingCard(tp,c100305031.cfilter,tp,LOCATION_HAND,0,1,1,nil)
 	Duel.ConfirmCards(1-tp,cg)
-	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+	if not Duel.IsPlayerAffectedByEffect(tp,EFFECT_DISCARD_COST_CHANGE) then 
+		Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+	end
 	Duel.PayLPCost(tp,1000)
 end
 function c100305031.target(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -38,20 +41,23 @@ function c100305031.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 end
 function c100305031.spfilter(c,e,tp)
-	return c:IsSetCard(0x208) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return (c:IsSetCard(0x208) or c:IsCode(18036057,69514125,76925842,12510878)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c100305031.activate(e,tp,eg,ep,ev,re,r,rp)
-	if not Duel.NegateActivation(ev) then return end
-	if re:GetHandler():IsRelateToEffect(re) and Duel.SendtoDeck(eg,nil,2,REASON_EFFECT)~=0 then
-		local loc=0
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then loc=loc+LOCATION_DECK end
-		if Duel.GetLocationCountFromEx(tp)>0 then loc=loc+LOCATION_EXTRA end
-		local g=Duel.GetMatchingGroup(c100305031.spfilter,tp,loc,0,nil,e,tp)
-		if loc==0 and g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(100305031,0)) then
-			Duel.BreakEffect()
-			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-			local sg=g:Select(tp,1,1,nil)
-			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	local ec=re:GetHandler()
+	if Duel.NegateActivation(ev) and ec:IsRelateToEffect(re) then
+		ec:CancelToGrave()
+		if Duel.SendtoDeck(ec,nil,2,REASON_EFFECT)~=0 and ec:IsLocation(LOCATION_DECK+LOCATION_EXTRA) then
+			local loc=0
+			if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then loc=loc+LOCATION_DECK end
+			if Duel.GetLocationCountFromEx(tp)>0 then loc=loc+LOCATION_EXTRA end
+			local g=Duel.GetMatchingGroup(c100305031.spfilter,tp,loc,0,nil,e,tp)
+			if loc~=0 and g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(100305031,0)) then
+				Duel.BreakEffect()
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+				local sg=g:Select(tp,1,1,nil)
+				Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+			end
 		end
 	end
 end
