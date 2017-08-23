7b53b10722cf22a74ce894816488a5344e35f47c
EerieCode <eeriecode@gmail.com>
2017-08-23 13:09:55 +0200

    Correct number for "Trap Crawling on World Legacy"
    36329071 -> 16329071

--- script/c16329071.lua
+++ script/c16329071.lua
@@ -1,30 +1,30 @@
 --星遺物に蠢く罠
 --Trap Crawling on the Starrelic
 --Script by nekrozar
-function c36329071.initial_effect(c)
+function c16329071.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,36329071+EFFECT_COUNT_CODE_OATH)
+	e1:SetCountLimit(1,16329071+EFFECT_COUNT_CODE_OATH)
 	e1:SetHintTiming(0,TIMING_END_PHASE)
-	e1:SetTarget(c36329071.target)
-	e1:SetOperation(c36329071.activate)
+	e1:SetTarget(c16329071.target)
+	e1:SetOperation(c16329071.activate)
 	c:RegisterEffect(e1)
 end
-function c36329071.filter(c)
-	return c:IsSetCard(0xfe) and not c:IsCode(36329071) and (c:IsLocation(LOCATION_HAND+LOCATION_GRAVE) or c:IsFaceup()) and c:IsAbleToDeck()
+function c16329071.filter(c)
+	return c:IsSetCard(0xfe) and not c:IsCode(16329071) and (c:IsLocation(LOCATION_HAND+LOCATION_GRAVE) or c:IsFaceup()) and c:IsAbleToDeck()
 end
-function c36329071.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g=Duel.GetMatchingGroup(c36329071.filter,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_ONFIELD+LOCATION_REMOVED,0,nil)
+function c16329071.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(c16329071.filter,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_ONFIELD+LOCATION_REMOVED,0,nil)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,2)
 		and g:GetClassCount(Card.GetCode)>=5 end
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,5,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
 end
-function c36329071.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c36329071.filter),tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_ONFIELD+LOCATION_REMOVED,0,nil)
+function c16329071.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c16329071.filter),tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_ONFIELD+LOCATION_REMOVED,0,nil)
 	if g:GetClassCount(Card.GetCode)<5 then return end
 	local sg=Group.CreateGroup()
 	for i=1,5 do
