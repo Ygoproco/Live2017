3f041eced3957782e9b32e96dee556d8e77addbf
EerieCode <eeriecode@gmail.com>
2017-09-25 22:04:24 +0200

    Official IDs
    Structure Deck R: Surge of Divine Light

--- script/c53666449.lua
+++ script/c53666449.lua
@@ -1,22 +1,22 @@
 --天空賢者ミネルヴァ
 --Angel Sage Minerva
 --Scripted by Eerie Code
-function c100305002.initial_effect(c)
+function c53666449.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_CHAIN_SOLVED)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCondition(c100305002.atkcon)
-	e1:SetOperation(c100305002.atkop)
+	e1:SetCondition(c53666449.atkcon)
+	e1:SetOperation(c53666449.atkop)
 	c:RegisterEffect(e1)
 end
-function c100305002.atkcon(e,tp,eg,ep,ev,re,r,rp)
+function c53666449.atkcon(e,tp,eg,ep,ev,re,r,rp)
 	return re:GetHandler():IsType(TYPE_COUNTER)
 end
-function c100305002.thfilter(c,cc)
+function c53666449.thfilter(c,cc)
 	return c:IsType(TYPE_COUNTER) and not c:IsCode(cc) and c:IsAbleToHand()
 end
-function c100305002.atkop(e,tp,eg,ep,ev,re,r,rp)
+function c53666449.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -27,14 +27,14 @@ function c100305002.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local rc=re:GetHandler()
 	if not rc then return end
 	local cc=rc:GetCode()
-	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c100305002.thfilter),tp,LOCATION_GRAVE,0,nil,cc)
-	if Duel.IsExistingMatchingCard(c100305002.sanctfilter,0,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) or (Duel.IsEnvironment(56433456)) and g:GetCount()>0 then
-		Duel.Hint(HINT_CARD,0,100305002)
+	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(c53666449.thfilter),tp,LOCATION_GRAVE,0,nil,cc)
+	if Duel.IsExistingMatchingCard(c53666449.sanctfilter,0,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) or (Duel.IsEnvironment(56433456)) and g:GetCount()>0 then
+		Duel.Hint(HINT_CARD,0,53666449)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local sg=g:Select(tp,1,1,nil)
 		Duel.SendtoHand(sg,nil,REASON_EFFECT)
 	end
 end
-function c100305002.sanctfilter(c)
+function c53666449.sanctfilter(c)
 	return c:IsFaceup() and c:IsCode(56433456)
 end
