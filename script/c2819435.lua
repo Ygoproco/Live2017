99f4098694ada1894d23b60b162146ccdb4562d2
Steeldarkeagel <steeldarkeagel@gmail.com>
2017-09-28 10:09:24 +0100

    Pacifis, the Phantasm City
    Tertiary fixes for Token

--- script/c2819435.lua
+++ script/c2819435.lua
@@ -89,12 +89,12 @@ function c2819435.thop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c2819435.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return rp~=tp and not Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_MZONE,0,1,nil,TYPE_TOKEN) and e:GetHandler():GetFlagEffect(1)>0
+	return rp~=tp and not Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_MZONE,0,1,nil,TYPE_TOKEN) and e:GetHandler():GetFlagEffect(1)>0 and e:GetHandler():GetFlagEffect(2819435)==0 and e:GetHandler():RegisterFlagEffect(2819435,RESET_EVENT+0x1fe0000+RESET_CHAIN,0,1)
 end
 function c2819435.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsPlayerCanSpecialSummonMonster(tp,2819436,0xfa,0x4011,2000,2000,6,RACE_WYRM,ATTRIBUTE_WATER)
-		and e:GetHandler():GetFlagEffect(2819435)==0 and not e:GetHandler():IsStatus(STATUS_CHAINING) end
+		and e:GetHandler():GetFlagEffect(2819435)==0 end
 		e:GetHandler():RegisterFlagEffect(2819435,RESET_EVENT+0x1fe0000+RESET_CHAIN,0,1)
 	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,0)
