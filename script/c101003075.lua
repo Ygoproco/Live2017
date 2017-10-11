e1a27e9625f7cce2d191c1bbcf4a29cfe7538226
Steelren <naimsantos45@gmail.com>
2017-10-11 17:57:49 -0300

    Fix for Archfiend's Call
    Now triggers Dark World monsters

--- script/c101003075.lua
+++ script/c101003075.lua
@@ -64,7 +64,7 @@ function c101003075.spop(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<1 or not tc:IsRelateToEffect(e) then return end
-	if Duel.DiscardHand(tp,c101003075.cfilter,1,1,REASON_EFFECT,nil)~=0 then
+	if Duel.DiscardHand(tp,c101003075.cfilter,1,1,REASON_EFFECT+REASON_DISCARD,nil)~=0 then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
