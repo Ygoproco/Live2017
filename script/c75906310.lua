6cfb9831471cbab246beb0c362097701c8ecdd91
EerieCode <eeriecode@gmail.com>
2017-11-12 10:27:52 +0100

    Official numbers for DP19

--- script/c75906310.lua
+++ script/c75906310.lua
@@ -1,7 +1,7 @@
 --アームド・ドラゴン・カタパルトキャノン
 --Armed Dragon Catapult Cannon
 --Scripted by Eerie Code
-function c100407021.initial_effect(c)
+function c75906310.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	aux.AddFusionProcCode2(c,84243274,73879377,true,true)
@@ -10,7 +10,7 @@ function c100407021.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(c100407021.splimit)
+	e1:SetValue(c75906310.splimit)
 	c:RegisterEffect(e1)
 	--special summon rule
 	local e2=Effect.CreateEffect(c)
@@ -18,8 +18,8 @@ function c100407021.initial_effect(c)
 	e2:SetCode(EFFECT_SPSUMMON_PROC)
 	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e2:SetRange(LOCATION_EXTRA)
-	e2:SetCondition(c100407021.spcon)
-	e2:SetOperation(c100407021.spop)
+	e2:SetCondition(c75906310.spcon)
+	e2:SetOperation(c75906310.spop)
 	c:RegisterEffect(e2)
 	--inactivate
 	local e3=Effect.CreateEffect(c)
@@ -28,86 +28,87 @@ function c100407021.initial_effect(c)
 	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(0,1)
-	e3:SetValue(c100407021.aclimit)
+	e3:SetValue(c75906310.aclimit)
 	c:RegisterEffect(e3)
 	--banish
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(100407021,0))
+	e4:SetDescription(aux.Stringid(75906310,0))
 	e4:SetCategory(CATEGORY_REMOVE)
 	e4:SetType(EFFECT_TYPE_QUICK_O)
 	e4:SetCode(EVENT_FREE_CHAIN)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(1)
 	e4:SetHintTiming(0,0x1e0)
-	e4:SetCondition(c100407021.rmcon)
-	e4:SetCost(c100407021.rmcost)
-	e4:SetTarget(c100407021.rmtg)
-	e4:SetOperation(c100407021.rmop)
+	e4:SetCondition(c75906310.rmcon)
+	e4:SetCost(c75906310.rmcost)
+	e4:SetTarget(c75906310.rmtg)
+	e4:SetOperation(c75906310.rmop)
 	c:RegisterEffect(e4)
 	--register summon
-	if not c100407021.global_flag then
-		c100407021.global_flag=true
+	if not c75906310.global_flag then
+		c75906310.global_flag=true
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		ge1:SetCode(EVENT_SPSUMMON_SUCCESS)
-		ge1:SetOperation(c100407021.regop)
+		ge1:SetOperation(c75906310.regop)
 		Duel.RegisterEffect(ge1,tp)
 	end
 end
-function c100407021.regop(e,tp,eg,ep,ev,re,r,rp)
+function c75906310.regop(e,tp,eg,ep,ev,re,r,rp)
 	for tc in aux.Next(eg) do
 		if tc:IsCode(84243274) then
-			Duel.RegisterFlagEffect(tc:GetControler(),100407021,0,0,0)
+			Duel.RegisterFlagEffect(tc:GetControler(),75906310,0,0,0)
 		elseif tc:IsCode(73879377) then
-			Duel.RegisterFlagEffect(tc:GetControler(),100407021+100,0,0,0)
+			Duel.RegisterFlagEffect(tc:GetControler(),75906311,0,0,0)
 		end
 	end
 end
-function c100407021.splimit(e,se,sp,st)
+function c75906310.splimit(e,se,sp,st)
 	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
 end
-function c100407021.spfilter(c,code)
+function c75906310.spfilter(c,code)
 	return c:IsFusionCode(code) and c:IsAbleToRemoveAsCost() and (c:IsFaceup() or c:IsLocation(LOCATION_GRAVE))
 end
-function c100407021.spcon(e,c)
+function c75906310.spcon(e,c)
 	if c==nil then return true end 
 	local tp=c:GetControler()
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>-3
-		and Duel.IsExistingMatchingCard(c100407021.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,nil,84243274)
-		and Duel.IsExistingMatchingCard(c100407021.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,nil,73879377)
-		and Duel.GetFlagEffect(tp,100407021)~=0 and Duel.GetFlagEffect(tp,100407021+100)~=0
+		and Duel.IsExistingMatchingCard(c75906310.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,nil,84243274)
+		and Duel.IsExistingMatchingCard(c75906310.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,nil,73879377)
+		and Duel.GetFlagEffect(tp,75906310)~=0 and Duel.GetFlagEffect(tp,75906311)~=0
 end
-function c100407021.spop(e,tp,eg,ep,ev,re,r,rp,c)
+function c75906310.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g1=Duel.SelectMatchingCard(tp,c100407021.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,1,nil,84243274)
+	local g1=Duel.SelectMatchingCard(tp,c75906310.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,1,nil,84243274)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g2=Duel.SelectMatchingCard(tp,c100407021.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,1,nil,73879377)
+	local g2=Duel.SelectMatchingCard(tp,c75906310.spfilter,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,1,nil,73879377)
 	g1:Merge(g2)
 	Duel.Remove(g1,POS_FACEUP,REASON_COST)
 end
-function c100407021.acfilter(c,cd)
+function c75906310.acfilter(c,cd)
 	return c:IsFaceup() and c:IsCode(cd)
 end
-function c100407021.aclimit(e,re,tp)
-	return Duel.IsExistingMatchingCard(c100407021.acfilter,e:GetHandlerPlayer(),LOCATION_REMOVED,LOCATION_REMOVED,1,nil,re:GetHandler():GetCode())
+function c75906310.aclimit(e,re,tp)
+	return Duel.IsExistingMatchingCard(c75906310.acfilter,e:GetHandlerPlayer(),LOCATION_REMOVED,LOCATION_REMOVED,1,nil,re:GetHandler():GetCode())
 end
-function c100407021.rmcon(e,tp,eg,ep,ev,re,r,rp)
+function c75906310.rmcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()~=tp
 end
-function c100407021.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c75906310.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemoveAsCost,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemoveAsCost,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,1,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
-function c100407021.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c75906310.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD+LOCATION_GRAVE,nil)
 	if chk==0 then return g:GetCount()>0 end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,g:GetCount(),0,0)
 end
-function c100407021.rmop(e,tp,eg,ep,ev,re,r,rp)
+function c75906310.rmop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,0,LOCATION_ONFIELD+LOCATION_GRAVE,nil)
 	if g:GetCount()>0 then
 		Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
 	end
 end
+
